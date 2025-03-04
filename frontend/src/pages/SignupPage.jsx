65b8ff410fad3c063b82e0003c222190174f8979
zainejaz67 <zain.ejaz67@gmail.com>
2025-03-04 21:32:25 +0500

    Sign Up page intial

---
+++ frontend/src/pages/SignupPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SignupPage = () => {
+  return (<>
+    <div>
+      <div>
+
+      </div>
+    </div>
+
+    <div>
+
+    </div>
+    </> )
+}
+
+export default SignupPage
