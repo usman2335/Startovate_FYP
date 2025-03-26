24072d55a3159e12e082e11d93b124445ee81b0b
zainejaz67 <zain.ejaz67@gmail.com>
2025-03-27 00:43:56 +0500

    Template mapping component Research Questions

--- frontend/src/pages/TestPage.jsx
+++ frontend/src/pages/TestPage.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 // // // import CreateNewCanvasModal from "../components/CreateNewCanvasModal";
-import Template1 from "../components/Templates/Research Questions/Template1";
-import Template2 from "../components/Templates/Research Questions/Template2";
-import Template3 from "../components/Templates/Research Questions/Template3";
-import Template4 from "../components/Templates/Research Questions/Template4";
-import Template5 from "../components/Templates/Research Questions/Template5";
-import Template6 from "../components/Templates/Research Questions/Template6";
+import RQTemplate1 from "../components/Templates/Research Questions/RQTemplate1";
 // // // import Template8 from "../components/Templates/Problem Identification/Template8";
 // // // import Checklist1 from "../components/Checklist1";
 // // import ProblemInvestigation from "../components/Templates/Template3";
@@ -17,7 +12,7 @@ const TestPage = () => {
     <>
       <div>
         {/* <Checklist1></Checklist1> */}
-        <Template6></Template6>
+        <RQTemplate1></RQTemplate1>
         {/* <ProblemInvestigation></ProblemInvestigation> */}
         {/* <StakeholderMapping></StakeholderMapping> */}
       </div>
