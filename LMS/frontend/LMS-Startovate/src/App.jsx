462ca3488684b8422882eda3378060e5c0912403
zainejaz67 <zain.ejaz67@gmail.com>
2025-06-14 12:29:11 +0500

    LMS Student 80/100

--- LMS/frontend/LMS-Startovate/src/App.jsx
+++ LMS/frontend/LMS-Startovate/src/App.jsx
@@ -1,16 +1,21 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import DashboardLayout from "./layout/DashboardLayout";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import ContentDisplay from "./components/ContentDisplay";
+import CoursePlayer from "./pages/CoursePlayer";
+import NavBar from "./components/NavBar";
+import LMSLanding from "./pages/LMS_Landing"; // adjust the path as needed
 
-function App() {
+const App = () => {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<DashboardLayout />} />
-        </Routes>
-      </Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<LMSLanding />} />
+        <Route path="/courses" element={<ContentDisplay />} />
+        <Route path="/courses/:courseId" element={<CoursePlayer />} />
+      </Routes>
     </>
   );
-}
+};
 
 export default App;
