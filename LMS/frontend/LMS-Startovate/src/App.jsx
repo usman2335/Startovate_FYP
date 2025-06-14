1eb058e1af212301a04a53c790c089f598f6571f
Babar Shaheen <babarshaheen132@gmail.com>
2025-06-14 23:19:27 +0500

    Implemented Learning management system with only manage users

--- LMS/frontend/LMS-Startovate/src/App.jsx
+++ LMS/frontend/LMS-Startovate/src/App.jsx
@@ -1,15 +1,27 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import DashboardLayout from "./layout/DashboardLayout";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import { AuthProvider } from "./context/authContext.jsx";
+import ManageUsers from "./pages/AdminDashboard/manageUsers.jsx";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<DashboardLayout />} />
+          {/* Public Routes */}
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+
+          {/* Dashboard Layout with Nested Routes */}
+          <Route path="/" element={<DashboardLayout />}>
+            <Route index element={<h1>Welcome to Dashboard</h1>} />
+            <Route path="manageusers" element={<ManageUsers />} />
+          </Route>
         </Routes>
       </Router>
-    </>
+    </AuthProvider>
   );
 }
 
---
+++ LMS/frontend/LMS-Startovate/src/context/authContext.jsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const checkAuthStatus = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/api/users/me", {
+        withCredentials: true,
+      });
+      setUser(response.data.user);
+      console.log("user is", response.data.user);
+    } catch (error) {
+      console.log("User not authenticated");
+      setUser(null);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await axios.post(
+        "http://localhost:5000/api/users/logout",
+        {},
+        { withCredentials: true }
+      );
+      setUser(null);
+      window.location.href = "/login";
+    } catch (error) {
+      console.error("Logout failed:", error);
+    }
+  };
+
+  useEffect(() => {
+    console.log("AuthContext mounted, calling checkAuthStatus");
+    checkAuthStatus();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, checkAuthStatus, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
